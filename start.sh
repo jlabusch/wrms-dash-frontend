3f23d9b10a1a46e7be7c743bf28a46a0ea97fbf2
Jacques Labuschagne <jlabusch@acm.org>
2018-10-09 00:14:29 +0100

    Wait until DB is up before starting gunicorn

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 
 perl -pi -e 's/PUT_DB_PASS_HERE/'$(cat /opt/db/pgpass | tr -d '\n')'/' dashboard/settings.py
 
-./manage.py makemigrations && ./manage.py migrate
+until ./manage.py makemigrations
+do
+    sleep 3
+done
+./manage.py migrate
 
 gunicorn -w 4 --error-logfile - --log-level info -b 0.0.0.0:80 --env DJANGO_SETTINGS_MODULE=dashboard.settings dashboard.wsgi
 
